d7915e3cb9c0c24a177f12d24288231c961dc2b5
java <leikeen_yang@163.com>
2020-11-06 22:55:59 +0800

    初始化

---
+++ src/test/java/com/example/springbootdoker/SpringbootDokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
